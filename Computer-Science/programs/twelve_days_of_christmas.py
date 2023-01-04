48889de598e9abe17ff55648f233c16f8d758c70
gk41 <max.hitchings@gmail.com>
2023-01-04 00:31:11 +0000

    new HW

--- Computer-Science/programs/twelve_days_of_christmas.py
+++ Computer-Science/programs/twelve_days_of_christmas.py
@@ -4,7 +4,17 @@ giftList = ["partridge in a pear tree", "turtle doves", "french hens", "calling 
 giftList.reverse()
 
 for i in range(12):
+    if i ==0:
+        suffix = 'st'
+    elif i == 1:
+        suffix = 'nd'
+    elif i == 2:
+        suffix = 'rd'
+    else:
+        suffix = 'th'
+
+
     currGifts = giftList[12-(i+1):]
     if i == 1:
         giftList[-1] = 'and '+currGifts[-1]
-    print(f"on the {i+1}st day of christmas my true love gave to me {i+1 if i != 0 else 'a'} {f', {i} '.join(currGifts)}")
+    print(f"on the {i+1}{suffix} day of christmas my true love gave to me {i+1 if i != 0 else 'a'} {f', {i} '.join(currGifts)}")
